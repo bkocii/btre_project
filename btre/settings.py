de2fe993c822b08fe84590bda7fb62954560994b
bkocii <burimkoci@hotmail.com>
2023-01-14 09:24:41 +0100

    removed email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -141,12 +141,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'burimabs81@gmail.com'
-EMAIL_HOST_PASSWORD = 'grhkuijabfocgdom'
-EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *
